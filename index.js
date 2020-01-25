395b196e6ae30b2f07cfcd238ed4dc8d49f30422
Griffin Bauer <griffinrbauer@icloud.com>
2020-01-24 19:16:41 -0600

    Removed redundant function from index.js; moved to discordUtils.js

--- index.js
+++ index.js
@@ -41,32 +41,4 @@ client.registry
 	process.on('unhandledRejection', error => discordUtils.discordDevLogSend(client, config.devLogChannelId, error));
 
 
-
-
-
-function discordDevLogSend(logMessage) {
-
-	let discordDevLogChannel = client.channels.get('665000776279785513');
-
-	const errorReportEmbed = new Discord.MessageEmbed()
-		.setColor('#e63232')
-		.setTitle('An error has occoured.')
-		.setAuthor('Tomato', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-		.setDescription('An error has occoured in Tomato. Diagnostics are below.')
-		.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-		.addField('Error Type', `${logMessage.name}: ${logMessage.message}`)
-		.addBlankField()
-		.addField('Stacktrace', `${logMessage.stack}`)
-		.addField('Server ID of error origin', 'discordDevLogChannel', true)
-		.addField('Server ID of error origin', 'Some value here', true)
-		.setImage('https://i.imgur.com/wSTFkRM.png')
-		.setTimestamp()
-		.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-	discordDevLogChannel.send(errorReportEmbed);
-
-}
-
-
-
 client.login(config.token);
