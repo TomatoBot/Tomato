6a9e620378bac177935a7b4a07e1f899751c0f2e
Griffin Bauer <griffinrbauer@icloud.com>
2020-03-05 19:11:28 -0600

    Preperation for a special addition...

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 const winston = require('winston');
-const Discord = require('discord.js');
 const config = require('./config.json');
 const discordUtils = require('./discordUtils.js');
-
-
-const logger = winston.createLogger({
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({ filename: 'log' }),
-	],
-	format: winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
-});
 
 const client = new CommandoClient({
 	commandPrefix: config.prefix,
@@ -36,7 +26,7 @@ client.registry
 
 	client.on("ready", () => {
 
-		client.user.setActivity(`over ${client.guilds.size} guilds`, { type: 'WATCHING' });
+		client.user.setActivity(`${client.guilds.size} guilds`, { type: 'WATCHING' });
 
 
 	});
@@ -45,52 +35,6 @@ client.registry
 	client.on('warn', m => logger.log('warn', m));
 	client.on('error', m => discordUtils.discordDevLogSend(client, config.devLogChannelId, error));
 	process.on('unhandledRejection', error => discordUtils.discordDevLogSend(client, config.devLogChannelId, error));
-
-	client.on('guildCreate', newGuild => {
-
-
-	console.log(`Joined ${newGuild.name}.`)
-
-	const joinedGuildSystemChannel = newGuild.systemChannel;
-
-	const tomatoWelcomingEmbed = new Discord.MessageEmbed()
-
-		.setColor('#0099ff')
-		.setTitle('Welcome!')
-		.setURL('https://example.com')
-		.setAuthor('Tomato', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-		.setDescription(`Hello there! Thank you for adding Tomato to ${newGuild.name}!\n\nTo get started, visit the dashboard at https://example.com.\nYou can also get a list of commands with #help.\nThank you, we hope you enjoy using Tomato! :)`)
-		.setTimestamp()
-		.setFooter('Tomato', 'https://i.imgur.com/wSTFkRM.png');
-
-		joinedGuildSystemChannel.send(tomatoWelcomingEmbed);
-
-
-	});
-
-	client.on('guildDelete', leftGuild => {
-
-		console.log(`Left ${newGuild.name}.`)
-
-
-
-	});
-
-
-	client.on('message', message => {
-
-		if (message.channel.type === 'dm') {
-
-			console.log(`[DM] From ${message.author.username} to ${message.channel.recipient.username}: ${message.content}`)
-
-
-		}
-
-
-	})
-
-
-
 
 
 client.login(config.token);
